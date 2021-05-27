cbbb4c8f3b29253b24fec79f2afc3f10586d56c3
gina7484 <gina7484@naver.com>
2021-05-28 02:08:49 +0900

    load input url's pdf

--- custom_paper.js
+++ custom_paper.js
@@ -1,6 +1,7 @@
 // If absolute URL from the remote server is provided, configure the CORS
 // header on that server.
-var url = "https://arxiv.org/pdf/1512.03385.pdf";
+
+var url = sessionStorage.getItem("paperURL");
 
 // Loaded via <script> tag, create shortcut to access PDF.js exports.
 var pdfjsLib = window["pdfjs-dist/build/pdf"];
--- lamp.js
+++ lamp.js
@@ -5,5 +5,14 @@ d3.select("#vgg-area").on("click", () => {
 d3.select("#add-paper-area")
     .select("img")
     .on("click", () => {
-        location.href = "./custom_paper.html";
+        let inputPaperField = d3.select("#submit-paper-field");
+        inputPaperField.attr("fold") == "true"
+            ? inputPaperField.classed("fold", false).attr("fold", false)
+            : inputPaperField.classed("fold", true).attr("fold", true);
     });
+
+d3.select("#submit-paper").on("click", () => {
+    let paperURL = document.getElementById("paper-url").value;
+    sessionStorage.setItem("paperURL", paperURL);
+    location.href = "./custom_paper.html";
+});
