d576d7e94e6abb384d2acea40d633677c94e7d45
gina7484 <gina7484@naver.com>
2021-05-28 02:51:16 +0900

    output mouse hovering done in vgg too

--- vgg.js
+++ vgg.js
@@ -168,8 +168,8 @@ d3.select("#conv-input")
     .on("mousemove", function (e) {
         tooltip.style("left", e.pageX - 7.5 + "px");
         tooltip.style("top", e.pageY - 7.5 + "px");
-        outputTooltip.style("left", e.pageX + "px");
-        outputTooltip.style("top", e.pageY + "px");
+        outputTooltip.style("left", e.pageX - 7.5 + 530 + "px");
+        outputTooltip.style("top", e.pageY - 7.5 + "px");
     })
     .on("click", function (event) {
         let clickedX = event.offsetX;
