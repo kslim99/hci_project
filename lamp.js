a92d0780e3776d4b13d87de3db27f37f3f60d35b
gina7484 <gina7484@naver.com>
2021-05-27 23:48:32 +0900

    made custom page for new papers

--- lamp.js
+++ lamp.js
@@ -1,3 +1,9 @@
-document.getElementById("vgg-area").addEventListener("click", () => {
+d3.select("#vgg-area").on("click", () => {
     location.href = "./vgg.html";
 });
+
+d3.select("#add-paper-area")
+    .select("img")
+    .on("click", () => {
+        location.href = "./custom_paper.html";
+    });
