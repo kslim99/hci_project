e8b96f29ec4bdaa6465d232a4ea101c48a305ece
gina7484 <gina7484@naver.com>
2021-06-06 15:27:31 +0900

    delete model4 & add googlenet

--- custom_paper.js
+++ custom_paper.js
@@ -136,16 +136,12 @@ d3.select("#chagne2vgg").on("click", () => {
     location.href = "./vgg.html";
 });
 
-d3.select("#change2model2").on("click", () => {
-    location.href = "./model2.html";
+d3.select("#change2alexnet").on("click", () => {
+    location.href = "./alexnet.html";
 });
 
-d3.select("#change2model3").on("click", () => {
-    location.href = "./model3.html";
-});
-
-d3.select("#change2model4").on("click", () => {
-    location.href = "./model4.html";
+d3.select("#change2googlenet").on("click", () => {
+    location.href = "./googlenet.html";
 });
 
 // -------------- link-to-paper --------------
